0da4430b4f14e41aca2f83a258ea2f0fb009200e
Hanitha Ganisetti <149133841+hanitha9@users.noreply.github.com>
2025-06-18 17:33:40 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,20 +20,22 @@ export default function HeroSection() {
   // Text Fill Animation for First Paragraph
   useAnimation(
     (target) => {
-      const words = target.querySelectorAll('.word');
-      return gsap.fromTo(
-        words,
-        { opacity: 0.3 },
-        {
-          opacity: 1,
-          duration: 0.5,
-          delay: (index) => index * 0.1,
-          scrollTrigger: {
-            trigger: target,
-            start: 'top 80%',
-          },
-        }
-      );
+      if (target) {
+        const words = target.querySelectorAll('.word');
+        return gsap.fromTo(
+          words,
+          { opacity: 0.3 },
+          {
+            opacity: 1,
+            duration: 0.5,
+            delay: (index) => index * 0.1,
+            scrollTrigger: {
+              trigger: target,
+              start: 'top 80%',
+            },
+          }
+        );
+      }
     },
     paraRef
   );

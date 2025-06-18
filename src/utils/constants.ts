b6fca8036114253abcd5366a79a16c2dd935ae2a
Hanitha Ganisetti <149133841+hanitha9@users.noreply.github.com>
2025-06-18 09:12:59 +0530

    Create constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+export const PRODUCTS = [
+  {
+    id: '1',
+    name: 'ALYA SKIN CLEANSER',
+    price: '$25.00',
+    description: 'Chair of the Health Training Committee',
+    image: '/product-1.jpg'
+  },
+  {
+    id: '2',
+    name: 'RITUAL OF SAKURA',
+    price: '$25.00',
+    description: 'HIGHWAY: 20-30 days',
+    image: '/product-2.jpg'
+  },
+  {
+    id: '3',
+    name: 'THE BODY LOTION',
+    price: '$25.00',
+    description: 'Natural skin texture',
+    image: '/product-3.jpg'
+  }
+];
